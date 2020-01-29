cc5e939ccb429a208d36b7062674e66fba861741
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-01-29 13:56:26 -0500

    first commit

---
+++ warmup1/sleepIn.js
@@ -0,0 +1,23 @@
+// Warmup-1 -- sleepIn
+// The parameter weekday is true if it is a weekday, 
+// and the parameter vacation is true if we are on vacation. 
+// We sleep in if it is not a weekday or we're on vacation.
+// Return true if we sleep in.
+
+// Examples
+
+// sleepIn(true, true) → true
+// sleepIn(true, false) → false
+// sleepIn(false, true) → true
+
+const sleepIn = (weekday, vacation) => {
+    if (weekday && !vacation) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(sleepIn(true, true))
+console.log(sleepIn(true, false))
+console.log(sleepIn(false, true))
---
+++ warmup1/sumDouble.js
@@ -0,0 +1,21 @@
+// Warmup-1 -- sumDouble
+// Given two int values, return their sum. Unless the two values 
+// are the same, then return double their sum.
+
+// Examples
+
+// sumDouble(1, 2) → 3
+// sumDouble(3, 2) → 5
+// sumDouble(2, 2) → 8
+
+const sumDouble = (a, b) => {
+    if (a === b) {
+        return (a + b) * 2;
+    } else {
+        return a + b;
+    }
+}
+
+console.log(sumDouble(1, 2))
+console.log(sumDouble(3, 2))
+console.log(sumDouble(2, 2))
